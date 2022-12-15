b42e7cc1d62384803d73ab932d821581dc1f56e7
Niy_g7 <114380216+E22CSEU0820@users.noreply.github.com>
2022-12-15 17:22:51 +0530

    niy_up3

--- niyati.py
+++ niyati.py
@@ -16,8 +16,13 @@ root.title("OneFile")
 img = ImageTk.PhotoImage(file = 'logo.png')
 root.iconphoto(False,img)
 
-root["background"]= "#4B4340"
+# root["background"]= "#4B4340"
 
+bg  = Image.open(r"logo.png")
+cop = bg.resize((250,250),Image.Resampling.LANCZOS)
+decomp = ImageTk.PhotoImage(cop)
+label = Label(root,image = decomp)
+label.place(x=330,y=20)
 
 text = "Select a file"
 
@@ -25,8 +30,8 @@ def compress():
     pass
 comp = ImageTk.PhotoImage(file=r"compress.png")
 b2 = Button(root,command=compress,height = 100,width = 150,image=comp)
-b2.place(x=60, y=300)
-lbl1 = Label(root,text="COMPRESS",font=("",10,"bold")).place(x=100,y=410)
+b2.place(x=60, y=350)
+# lbl1 = Label(root,text="COMPRESS",font=("",10,"bold")).place(x=100,y=410)
 
 
 
@@ -34,8 +39,8 @@ def decompress():
     pass
 decmp = ImageTk.PhotoImage(file=r"decompress.png") 
 b3 = Button(root,command=decompress,height = 100,width = 150,image=decmp)
-b3.place(x=270,y=300)
-lbl2 = Label(root,text="DECOMPRESS",font=("",10,"bold")).place(x=300,y=410)
+b3.place(x=270,y=350)
+# lbl2 = Label(root,text="DECOMPRESS",font=("",10,"bold")).place(x=300,y=410)
 
 
 
@@ -44,9 +49,9 @@ def encrypt():
     # sav()
 encrp = ImageTk.PhotoImage(file=r"encrypt.png")
 b4 = Button(root,text="ENCRYPT",command=encrypt,height = 100,width = 150,image=encrp)
-b4.place(x=480,y=300)
-lbl1 = Label(root,text="ENCRYPT",font=("",10,"bold"))
-lbl1.place(x=530,y=410)
+b4.place(x=480,y=350)
+# lbl1 = Label(root,text="ENCRYPT",font=("",10,"bold"))
+# lbl1.place(x=530,y=410)
 
 
 
@@ -55,8 +60,8 @@ def decrypt():
     # sav()
 decrp = ImageTk.PhotoImage(file=r"decrypt.png")
 b5 = Button(root,text="DECRYPT",command=decrypt,height = 100,width = 150,image=decrp)
-b5.place(x=690, y=300)
-lbl1 = Label(root,text="DECRYPT",font=("",10,"bold")).place(x=740,y=410)
+b5.place(x=690, y=350)
+# lbl1 = Label(root,text="DECRYPT",font=("",10,"bold")).place(x=740,y=410)
   
 
 
