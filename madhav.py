7f99ead734523ad6bc7c27716090e44c41fd47d5
M4dhav <114633095+M4dhav@users.noreply.github.com>
2022-12-15 17:46:29 +0530

    madhav_up3

--- madhav.py
+++ madhav.py
@@ -2,42 +2,45 @@ import os
 from cryptography.fernet import Fernet
 from save import sav
 from save import add_file
-from save import rot
+
 def encryption(filepath):
-    try:
-        filename = ""
-        fileexten = ""
-        for i in filepath[::-1]:
-            if i == chr(92):
-                break
-            filename += i
-        for i in filepath[::-1]:
-            if i == ".":
-                break
-            fileexten += i
-        fileexten = fileexten[::-1]
-        filename = filename[::-1]
-        filename = filename.replace(".","")
-        filename = filename.replace(fileexten, "")
-        filename += "_encrypted"
-        filename += "." + fileexten
-        key = Fernet.generate_key()
-        key_path = sav("Choose Key File Path (Please save as '.key' file)")
-        with open(key_path, "wb") as key_file:
-            key_file.write(key)
-        with open(filepath,"rb") as file:
-            content = file.read()
-        content_encrypted = Fernet(key).encrypt(content)
-        if rot():
-            with open(sav("Select Encrypted File Path"), "wb") as file:
-                file.write(content_encrypted)
-        else:
-            with open(filepath, "wb") as file:
-                file.write(content_encrypted)
-            return
-    except FileNotFoundError:
-        print("Please enter a valid file location.")
-        # encryption(add_file("Select a File"))
+    filename = ""
+    fileexten = ""
+    for i in filepath[::-1]:
+        if i == chr(92):
+            break
+        filename += i
+    for i in filepath[::-1]:
+        if i == ".":
+            break
+        fileexten += i
+    fileexten = fileexten[::-1]
+    filename = filename[::-1]
+    filename = filename.replace(".","")
+    filename = filename.replace(fileexten, "")
+    filename += "_encrypted"
+    filename += "." + fileexten
+    key = Fernet.generate_key()
+    key_path = sav("Choose Key File Path (Please save as '.key' file)")
+    # try:
+    #     open(key_path, "r")
+    # except FileNotFoundError:
+    #     print("Please select a valid key location.")
+    #     key_path = sav("Choose Key File Path (Please save as '.key' file)")
+    with open(key_path, "wb") as key_file:
+        key_file.write(key)
+    with open(filepath,"rb") as file:
+        content = file.read()
+    content_encrypted = Fernet(key).encrypt(content)
+    save_file_path = sav("Select Encrypted File Path")
+
+    with open(save_file_path, "wb") as file:
+        file.write(content_encrypted)
+    # else:
+    #     with open(filepath, "wb") as file:
+    #         file.write(content_encrypted)
+    #     return
+    
 
 def decryption(filepath):
     try:
--- save.py
+++ save.py
@@ -7,29 +7,29 @@ def sav(text):
 def add_file(text):
     d = filedialog.askopenfilename(initialdir="/",title=text,filetypes = (("Allfiles","*.*"),("TxT files","*.txt")))
     return d 
-def rot():
-    def dest():
-        top.destroy()
-    def both1():
-        dest()
-        return (1)
-    def both2():
-        dest()
-        return (0)
-    top = Toplevel()
-    top.geometry("300x200")
-    top.minsize(300,180)
-    top.maxsize(300,180)
-    label = Label(top,text='''Do you want a new encrypted copy of the file or
-    for the current copy to be encrypted?''')
-    label.place(x=25,y=0)
+# def rot():
+#     def dest():
+#         top.destroy()
+#     def both1():
+#         dest()
+#         return (1)
+#     def both2():
+#         dest()
+#         return (0)
+#     top = Toplevel()
+#     top.geometry("300x200")
+#     top.minsize(300,180)
+#     top.maxsize(300,180)
+#     label = Label(top,text='''Do you want a new encrypted copy of the file or
+#     for the current copy to be encrypted?''')
+#     label.place(x=25,y=0)
 
 
-    b9 = Button(top,text = "NEW",font =("",8,"bold"),height = 2,width = 8 ,command=both1)
-    b9.place(x =80 ,y=70)
+#     b9 = Button(top,text = "NEW",font =("",8,"bold"),height = 2,width = 8 ,command=both1)
+#     b9.place(x =80 ,y=70)
 
 
-    b2 = Button(top,text = "CURRENT",font =("",8,"bold"),height = 2, width=8,command=both2)
-    b2.place(x=170,y=70)
+#     b2 = Button(top,text = "CURRENT",font =("",8,"bold"),height = 2, width=8,command=both2)
+#     b2.place(x=170,y=70)
 
-    top.mainloop()
+#     top.mainloop()
