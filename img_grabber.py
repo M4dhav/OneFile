1fc76a59809e4a65809d10d7bbf01c394b49b22d
M4dhav <114633095+M4dhav@users.noreply.github.com>
2022-12-15 16:59:41 +0530

    up

--- img_grabber.py
+++ img_grabber.py
@@ -1,11 +1,15 @@
 import requests
-dic = {"compress": "https://ibb.co/bN0Rmq8", "decompress": "https://ibb.co/F6W4fC3", "decrypt":"https://github.com/M4dhav/open-file-winrar-alternative/blob/main/decrypt_icon.jpg", "encrypt":"https://github.com/M4dhav/open-file-winrar-alternative/blob/main/encrpyt_icon.jpg"}
+import os
+dic = {"compress": "https://i.ibb.co/frgGFBP/compress-icon.jpg", "decompress": "https://i.ibb.co/1ZnGpjQ/decompress-icon.jpg", "decrypt":"https://i.ibb.co/hL9qZMj/decrypt-icon.jpg", "encrypt":"https://i.ibb.co/prwryRc/encrpyt-icon.jpg", "logo": "https://i.ibb.co/ZgPWNdb/final-One-Filelogo.jpg"}
 keys = list(dic.keys())
 for i in keys:
     url = dic[i]
     file_name = i + ".png"
-    response = requests.get(url)
-    if response.status_code:
-        fp = open(file_name, 'wb')
-        fp.write(response.content)
-        fp.close()
+    if os.path.exists(file_name):
+        pass
+    else:
+        response = requests.get(url)
+        if response.status_code:
+            fp = open(file_name, 'wb')
+            fp.write(response.content)
+            fp.close()
--- niyati.py
+++ niyati.py
@@ -6,14 +6,14 @@ from PIL import ImageTk, Image
 from madhav import encryption as e
 from madhav import decryption as d
 from save import add_file
-
+import img_grabber
 root = Tk()
 root.geometry("900x550")
 root.maxsize(900,550)
 root.minsize(900,550)
 root.title("OneFile")
 
-img = ImageTk.PhotoImage(file = 'C:\\Users\\Dell\\Desktop\\Hackathon\\ico.png')
+img = ImageTk.PhotoImage(file = 'logo.png')
 root.iconphoto(False,img)
 
 # root["background"]= "#325984"
@@ -32,7 +32,7 @@ text = "Select a file"
 def compress():
     # sav()
     pass
-comp = ImageTk.PhotoImage(file=r"C:\Users\Dell\Desktop\Hackathon\compress.png")
+comp = ImageTk.PhotoImage(file=r"compress.png")
 b2 = Button(root,command=compress,height = 100,width = 150,image=comp)
 b2.place(x=60, y=300)
 lbl1 = Label(root,text="COMPRESS",font=("",10,"bold")).place(x=100,y=410)
@@ -40,7 +40,7 @@ lbl1 = Label(root,text="COMPRESS",font=("",10,"bold")).place(x=100,y=410)
 def decompress():
     # sav()
     pass
-decmp = ImageTk.PhotoImage(file=r"C:\Users\Dell\Desktop\Hackathon\2528903.png") 
+decmp = ImageTk.PhotoImage(file=r"decompress.png") 
 b3 = Button(root,command=decompress,height = 100,width = 150,image=decmp)
 b3.place(x=270,y=300)
 lbl2 = Label(root,text="DECOMPRESS",font=("",10,"bold")).place(x=300,y=410)
@@ -49,7 +49,7 @@ lbl2 = Label(root,text="DECOMPRESS",font=("",10,"bold")).place(x=300,y=410)
 def encrypt():
     e(add_file(text))
     # sav()
-encrp = ImageTk.PhotoImage(file=r"C:\Users\Dell\Desktop\Hackathon\625728.png")
+encrp = ImageTk.PhotoImage(file=r"encrypt.png")
 b4 = Button(root,text="ENCRYPT",command=encrypt,height = 100,width = 150,image=encrp)
 b4.place(x=480,y=300)
 lbl1 = Label(root,text="ENCRYPT",font=("",10,"bold")).place(x=530,y=410)
@@ -59,7 +59,7 @@ lbl1 = Label(root,text="ENCRYPT",font=("",10,"bold")).place(x=530,y=410)
 def decrypt():
     d(add_file(text))
     # sav()
-decrp = ImageTk.PhotoImage(file=r"C:\Users\Dell\Desktop\Hackathon\DFD-Decryption.webp")
+decrp = ImageTk.PhotoImage(file=r"decrypt.png")
 b5 = Button(root,text="DECRYPT",command=decrypt,height = 100,width = 150,image=decrp)
 b5.place(x=690, y=300)
 lbl1 = Label(root,text="DECRYPT",font=("",10,"bold")).place(x=740,y=410)
