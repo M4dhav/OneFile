5589599ea933b10f85ea7bef6416ee61cde2f9e4
M4dhav <114633095+M4dhav@users.noreply.github.com>
2022-12-15 18:02:20 +0530

    madhav_update_working

--- madhav.py
+++ madhav.py
@@ -43,43 +43,28 @@ def encryption(filepath):
     
 
 def decryption(filepath):
-    try:
-        filename = ""
-        fileexten = ""
-        for i in filepath[::-1]:
-            if i == chr(92):
-                break
-            filename += i
-        for i in filepath[::-1]:
-            if i == ".":
-                break
-            fileexten += i
-        fileexten = fileexten[::-1]
-        filename = filename[::-1]
-        filename = filename.replace(".","")
-        filename = filename.replace(fileexten, "")
-        filename = filename.replace("_encrypted", "_decrypted")
-        filename += "." + fileexten
-        key_path = add_file("Select Key File Path")
-        with open(key_path, "rb") as key_file:
-            key = key_file.read()
-            try:
-                os.remove(key_path)
-            except PermissionError:
-                pass
-        # choice = input("Do you want a new decrypted copy of the file or for the current copy to be decrypted?(New/Current)\n")
-        # possible_inputs = ["New", "new", "Current", "current"]
-        # while choice not in possible_inputs:
-        #     print("Please enter a valid choice.")
-        with open(filepath,"rb") as file:
-            content = file.read()
-        content_decrypted = Fernet(key).decrypt(content)
-        # if choice == "New" or choice == "new":
-        #     with open(filename,"wb") as file:
-        #         file.write(content_decrypted)
-        # else:
-        with open(sav(), "wb") as file:
+    filename = ""
+    fileexten = ""
+    for i in filepath[::-1]:
+        if i == chr(92):
+            break
+        filename += i
+    for i in filepath[::-1]:
+        if i == ".":
+            break
+        fileexten += i
+    fileexten = fileexten[::-1]
+    filename = filename[::-1]
+    filename = filename.replace(".","")
+    filename = filename.replace(fileexten, "")
+    filename = filename.replace("_encrypted", "_decrypted")
+    filename += "." + fileexten
+    key_path = add_file("Select Key File Path")
+    with open(key_path, "rb") as key_file:
+        key = key_file.read()
+    with open(filepath,"rb") as file:
+        content = file.read()
+    content_decrypted = Fernet(key).decrypt(content)
+    save_file_path = sav("Select Decrypted File Path")
+    with open(save_file_path, "wb") as file:
             file.write(content_decrypted)
-        
-    except FileNotFoundError:
-        print("Please enter a valid file location.")
